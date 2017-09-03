6dc9920c7a8652e1cdfd35999067916350b66571
Rick Milliken <rickmilliken@me.com>
2017-09-03 14:34:29 -0500

    added bootstrap library

--- TestCnx.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("myathensricorg.ipowermysql.com", "amsti_01", "Capstone@17", "amsti_01");   
-if ($conn -> connect_error) { die("Connection failed: " . $conn->connect_error); } 
-
-echo "Connected successfully";
-
-$conn->close();
-?>
