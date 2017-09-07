45db15436ec1d20ac0ed8e011fd0611db0bb80e4
Rick Milliken <rickmilliken@me.com>
2017-09-06 19:55:34 -0500

    Modified form to make fields dynamic, continue to add fields.

--- public_html/js/NewRequest.js
+++ public_html/js/NewRequest.js
@@ -1,22 +1,45 @@
 
-/*
-window.onload = function() {
-    document.getElementById('General').style.display = 'none';
-    document.getElementById('BookStudy').style.display = 'none';
+
+
+window.onload = initPage();
+
+function initPage() {
+
+
 }
-*/
+
+
+// Function to manipulate dom after content is loaded.
+window.addEventListener("load", function(){
+    document.getElementById('school_system_row').style.display = 'none';
+    document.getElementById('request_desc_row').style.display = 'none';
+    document.getElementById('book_title_row').style.display = 'none';
+    document.getElementById('format_method_row').style.display = 'block';
+
+    //document.getElementById('book_title_row').style.display = 'none';
+
+
+});
+
 
 function selectRequestType() {
     if(document.getElementById('General').checked) {
         // alert("You selected General");
+        document.getElementById('school_system_row').style.display = 'block';
         document.getElementById('request_desc_row').style.display = 'block';
+        document.getElementById('book_title_row').style.display = 'none';
+        document.getElementById('format_method_row').style.display = 'block';
     }
     else if (document.getElementById('BookStudy').checked) {
         // alert("You selected BookStudy");
+        document.getElementById('school_system_row').style.display = 'block';
         document.getElementById('request_desc_row').style.display = 'none';
+        document.getElementById('book_title_row').style.display = 'block';
+        document.getElementById('format_method_row').style.display = 'block';
+
     }
     else{
-        //document.getElementById('request_desc_row').style.display = 'inline';
+        // do nothing at the moment
     }
 }
 
