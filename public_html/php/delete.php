c3b3e88127dbe67a7dbfb957b2e8a7a011220171
Jason Verbosh <jason.verbosh@gmail.com>
2017-09-14 21:55:27 -0500

    Allow the admin to upload bylaws and newsletters. Also allow the admin to select what is the current of each

---
+++ public_html/php/delete.php
@@ -0,0 +1,44 @@
+<?php
+    // Delete a file from the database
+    require "../../resources/config.php";
+
+    $id = (int) $_GET['id'];
+    $table = $_GET['table'];
+
+    $db = $config['db']['admin_files'];
+    $link = new mysqli($db['host'], $db['username'], $db['password'], $db['dbname']) or die('There was a problem connecting to the database.');
+
+    $get_path_query = "SELECT * from $table WHERE id=$id";
+    $get_path = $link->query($get_path_query) or die($link->error);
+
+    // Delete file on the server
+    $row = mysqli_fetch_array($get_path);
+    $file_to_delete = $row['file_path'];
+
+    if (file_exists($file_to_delete)) {
+        if (unlink($file_to_delete)) {
+
+            // Delete database reference after file deletion
+            $query = "DELETE FROM $table WHERE id=$id LIMIT 1"; 
+            if ($stmt = $link->query($query) or die($link->error)) {
+                echo "<script type='text/javascript'>alert('File deleted!')</script>";
+            }
+            else {
+                echo "<script type='text/javascript'>alert('ERROR: The file reference was not deleted from the database.')</script>";
+            }
+        }
+        else {
+            echo "<script type='text/javascript'>alert('ERROR: The file was unable to be deleted.')</script>";
+        }
+    }
+    else {
+        // Delete database reference when file isn't stored locally anymore
+        $query = "DELETE FROM $table WHERE id=$id LIMIT 1"; 
+        if ($stmt = $link->query($query) or die($link->error)) {
+            echo "<script type='text/javascript'>alert('File was missing but the database reference has been removed.')</script>";
+        }
+    }
+
+    $link->close();
+    header('refresh: 0; URL=../Admin.php');
+?>
---
+++ public_html/php/upload_file.php
@@ -0,0 +1,45 @@
+<?php
+    require "admin_functions.php";
+
+    $table = $_GET['table'];
+    $type = $_GET['type'];
+    $dir = $_GET['dir'];
+
+    $uploaded_file = $_FILES[$type]["name"];
+
+    $target = "../Uploads/" . $dir . "/" . $uploaded_file;
+    $file_type = pathinfo($uploaded_file, PATHINFO_EXTENSION);
+
+    // Check that the user selected a file to upload
+    if (empty($_FILES[$type]['name'])) {
+        echo "<script type='text/javascript'>alert('Please select a file to upload.')</script>";
+    }
+
+    // Check if the file already exists
+    elseif (check_if_file_exists($table, $uploaded_file))
+    {         
+        echo "<script type='text/javascript'>alert('File $uploaded_file already exists. Please rename the file and try again.')</script>";
+    }
+
+    // Allow only PDF file formats
+    elseif($file_type != "pdf") {
+        echo "<script type='text/javascript'>alert('Only PDF files are able to be uploaded.')</script>";
+    }
+
+    // Try to upload the file
+    elseif (move_uploaded_file($_FILES[$type]["tmp_name"], $target)) {
+
+        if (add_file_to_database($table, $uploaded_file, $target)) {
+            echo "<script type='text/javascript'>alert('$uploaded_file was successfully uploaded')</script>";
+        }
+        else
+        {
+            echo "<script type='text/javascript'>alert('$uploaded_file was NOT uploaded successfully')</script>";
+        }
+    }
+    else {
+        echo "<script type='text/javascript'>alert('$uploaded_file was NOT uploaded successfully')</script>";
+    }
+    
+    header('refresh: 0; URL=../Admin.php');
+?>
