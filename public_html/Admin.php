7ee1a72bf90163b4cd774aef2428ac8aff58b5e0
Jason Verbosh <jason.verbosh@gmail.com>
2017-09-18 17:08:00 -0500

    Changed title of the Admin page

--- public_html/Admin.php
+++ public_html/Admin.php
@@ -9,7 +9,7 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Professional Development Opportunities</title>
+        <title>Admin</title>
         
         <link rel="stylesheet" href="css/foundation.min.css">
         <link rel="stylesheet" href="css/Common.css" />
