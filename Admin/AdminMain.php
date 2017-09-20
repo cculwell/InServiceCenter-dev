8ee8a8093311cbae20cc47107bd531b4645780b2
Jason Verbosh <jason.verbosh@gmail.com>
2017-09-20 00:49:32 -0500

    Added 'Under construction' label to main admin page and fixed textarea element alignment

--- Admin/AdminMain.php
+++ Admin/AdminMain.php
@@ -1,10 +1,21 @@
 <?php include "MenuBar.html"; ?>
 
 <!DOCTYPE html>
-<html>
+<html class="no-js" lang="en" dir="ltr">
+
     <head>
-        <link rel="stylesheet" href="css/MenuBar.css">
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Home</title>
+
+        <link rel="stylesheet" href="css/Admin.css" />
     </head>
+
     <body>
+    	<div class="page_container"> 
+    		<h3>Welcome to the Admin Page. This page is still under construction</h3>
+    	</div> 
     </body>
 </html>
