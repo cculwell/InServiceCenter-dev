8f6f16ca1ad6e22302c3e96f3ae5a82d0a0e87f6
Jason Verbosh <jason.verbosh@gmail.com>
2017-11-08 22:50:10 -0600

    Use variable for filename instead of the name each time

--- public_html/js/reports/financial_report.js
+++ public_html/js/reports/financial_report.js
@@ -42,14 +42,14 @@ $(document).ready(function() {
                         {
                             if (req.readyState === 4 && req.status === 200) 
                             {
-                                var filename = "Financial Report-" + new Date().getTime() + ".pdf";
+                                var filename = "Financial Report.pdf";
                                 if (typeof window.chrome !== 'undefined') 
                                 {
                                     // Chrome version
                                     var link = document.createElement('a');
 
                                     link.href = window.URL.createObjectURL(req.response);
-                                    link.download = "Financial Report-" + new Date().getTime() + ".pdf";
+                                    link.download = filename;
                                     link.click();
                                 } 
                                 else if (typeof window.navigator.msSaveBlob !== 'undefined') 
